49e06cb7ee3b26fb8dc4706f12b63f32460d5f2a
nexlight101 <henniejpienaar@gmail.com>
2020-03-19 15:47:59 +0200

    Fisrt commit of gRPC repo

---
+++ greet/greet_server/go.mod
@@ -0,0 +1,5 @@
+module github.com/nexlight101/grpc-go-course/greet/greetpb/greet_server
+
+go 1.14
+
+require google.golang.org/grpc v1.28.0 // indirect
---
+++ greet/greet_server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/nexlight101/grpc-go-course/greet/greetpb"
+	"google.golang.org/grpc"
+)
+
+// Create server struct
+type server struct{}
+
+func main() {
+	fmt.Println("Hello gRPC")
+
+	//  Create a listener at port 50051.
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v ", err)
+	}
+	// Create a new grpc server.
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+	// Serve the listener.
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
